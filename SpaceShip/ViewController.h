3b5ad281cb8b1c3d874f8f25af968eab63a0b11c
yu <jimyu0414@gmail.com>
2015-02-12 13:46:17 +1100

    Initial Commit

---
+++ SpaceShip/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SpaceShip
+//
+//  Created by yu on 12/02/2015.
+//  Copyright (c) 2015 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SpaceShip/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SpaceShip
+//
+//  Created by yu on 12/02/2015.
+//  Copyright (c) 2015 yu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
